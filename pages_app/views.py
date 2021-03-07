7089c6b2166af1b2a93a177146817457ebf34f42
Rinku97 <rinkurb0@gmail.com>
2021-03-07 15:51:54 +0530

    added the functionality for listings

--- pages_app/views.py
+++ pages_app/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from listing_app.models import Listing
+from listing_app.choices import price_choices, state_choices, category_choices
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'pages_app/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings,
+        'price_choices': price_choices,
+        'state_choices': state_choices,
+        'category_choices': category_choices,
+    }
+    return render(request, 'pages_app/index.html', context)
 
 
 def about(request):
